67260cffbbbb7f1ba0e3ffa88d1c8b18ef83b5b8
kachithe1st <105376823+kachithe1st@users.noreply.github.com>
2022-06-25 15:22:50 +0100

    Update life.js

--- life.js
+++ life.js
@@ -1,4 +1,5 @@
 
+
 var a = +prompt("Enter first number");
 var b = +prompt("Enter second number");
 const operator = prompt("enter math symbol (+,*,/,-)");
@@ -13,10 +14,10 @@ if(operator == "+"){
     result = a / b;
 } else if(operator == "-"){
     result = a - b;
-}else{
-    alert("please put math symbol");
-    alert("solution will be undefined");
+} else{
+    alert("please put math symbol or solution will be undefined");
     alert("cancel and refresh page");
+}
 // document.write(a + operator + b + '=' + result)
 // the above in comment would enable the result to be displayed as text in the webpage but instead i want as a prompt
 alert(a + operator + b + '=' + result);
